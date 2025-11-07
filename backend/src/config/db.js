f23d1a76cffd89c2f0366c68c8bb06a1a93a613c
SadicStroke <bautistaaiello@hotmail.com.ar>
2025-11-07 16:44:09 -0300

    git commit -m ":p"

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,9 +5,9 @@ dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ Conectado a MongoDB");
+    console.log("Conectado a MongoDB");
   } catch (error) {
-    console.error("❌ Error al conectar a MongoDB:", error.message);
+    console.error("Error al conectar a MongoDB:", error.message);
     process.exit(1);
   }
 };
